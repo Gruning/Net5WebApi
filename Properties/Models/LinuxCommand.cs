92085e32fe45ede1b22311aa0f6960487f607f66
Alberto Gruning <alberto.gruning.zen@gmail.com>
2020-09-09 18:39:36 -0300

    17_27_models

---
+++ Properties/Models/LinuxCommand.cs
@@ -0,0 +1,8 @@
+namespace Net5WebApi.Models
+{
+    public class LinuxCommand{
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Sentence { get; set; }
+    }
+}
