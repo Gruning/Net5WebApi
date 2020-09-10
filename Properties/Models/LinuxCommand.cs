534db108c823dd96b00093c54a865f29797b8891
Alberto Gruning <alberto.gruning.zen@gmail.com>
2020-09-10 19:13:32 -0300

    first model

--- Properties/Models/LinuxCommand.cs
+++ Properties/Models/LinuxCommand.cs
@@ -4,5 +4,6 @@ namespace Net5WebApi.Models
         public int Id { get; set; }
         public string HowTo { get; set; }
         public string Sentence { get; set; }
+        public string Platform { get; set; }
     }
 }
