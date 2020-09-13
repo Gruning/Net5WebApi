f1665d4c4ec56405f675e86a54db5bcabc945479
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-13 19:56:59 -0300

    snippet

---
+++ Models/Snippet.cs
@@ -0,0 +1,8 @@
+namespace Net5WebApi.Models{
+    public class Snippet{
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }   
+    }
+}
