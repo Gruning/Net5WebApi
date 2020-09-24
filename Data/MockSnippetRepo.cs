70efd629c88e7cd660da05ce396bae9aefe6adc8
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-24 20:15:18 -0300

    mockSnippetRepo

--- Data/MockSnippetRepo.cs
+++ Data/MockSnippetRepo.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+using Net5WebApi.Models;
+
 namespace Net5WebApi.Data
 {
     public class MockSnippetRepo : ISnippetRepository{
-
+        public IEnumerable<Snippet> GetAllSnippets(){
+            return null; 
+        }
+        public Snippet GetSnippetById(int id){
+            return new Snippet{id =0} 
+        }
     }
 }
