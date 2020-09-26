7ad395fe71dbedbc5ae6fb1a881cefcc452ee810
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-26 13:03:58 -0300

    mock repo

--- Data/MockSnippetRepo.cs
+++ Data/MockSnippetRepo.cs
@@ -5,10 +5,30 @@ namespace Net5WebApi.Data
 {
     public class MockSnippetRepo : ISnippetRepository{
         public IEnumerable<Snippet> GetAllSnippets(){
-            return null; 
+            return new List<Snippet>{
+                    new Snippet{
+                id =1, 
+                HowTo="last argument"
+                Line="!$"
+                Platform="bash"},
+                    new Snippet{
+                id =1, 
+                HowTo="current folder"
+                Line=" ./ "
+                Platform="bash"},
+                    new Snippet{
+                id =1, 
+                HowTo="parent folder"
+                Line=" ../ "
+                Platform="bash"},
+            }; 
         }
         public Snippet GetSnippetById(int id){
-            return new Snippet{id =0} 
+            return new Snippet{
+                id =0, 
+                HowTo="go to prev. folder"
+                Line="cd -"
+                Platform="bash"} 
         }
     }
 }
