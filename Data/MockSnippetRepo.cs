516d772c83664b13f3b412bc979e5d3fce99aaf9
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-26 18:58:36 -0300

    bug fix

--- Data/MockSnippetRepo.cs
+++ Data/MockSnippetRepo.cs
@@ -7,28 +7,28 @@ namespace Net5WebApi.Data
         public IEnumerable<Snippet> GetAllSnippets(){
             return new List<Snippet>{
                     new Snippet{
-                id =1, 
-                HowTo="last argument"
-                Line="!$"
+                Id =1, 
+                HowTo="last argument",
+                Line="!$",
                 Platform="bash"},
                     new Snippet{
-                id =1, 
-                HowTo="current folder"
-                Line=" ./ "
+                Id =1, 
+                HowTo="current folder",
+                Line=" ./ ",
                 Platform="bash"},
                     new Snippet{
-                id =1, 
-                HowTo="parent folder"
-                Line=" ../ "
+                Id =1, 
+                HowTo="parent folder",
+                Line=" ../ ",
                 Platform="bash"},
             }; 
         }
         public Snippet GetSnippetById(int id){
             return new Snippet{
-                id =0, 
-                HowTo="go to prev. folder"
-                Line="cd -"
-                Platform="bash"} 
+                Id =0, 
+                HowTo="go to prev. folder",
+                Line="cd -",
+                Platform="bash"}; 
         }
     }
 }
